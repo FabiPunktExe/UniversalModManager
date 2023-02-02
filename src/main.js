635c25f8fc2a650ebeb0725e6b7cecd0034d951a
MrStupsi <MrStupsi@gmx.de>
2023-02-02 16:50:34 +0100

    much cool stuff

--- src/main.js
+++ src/main.js
@@ -1,20 +1,35 @@
-const { app, BrowserWindow } = require("electron");
+const { assert } = require("console");
+const { app, BrowserWindow, ipcMain, globalShortcut } = require("electron");
 const { join } = require("path");
+var window = null;
+
+function play(event, data) {
+    assert(window != null);
+    console.log("play: " + data);
+}
 
 function createWindow() {
-    const mainWindow = new BrowserWindow({
+    window = new BrowserWindow({
         webPreferences: {
-            nodeIntegration: true
+            nodeIntegration: true,
+            preload: join(__dirname, "preload.js")
         }
     });
-    mainWindow.maximize();
-    mainWindow.loadFile(join(__dirname, "index.html"));
+    window.setMenuBarVisibility(true);
+    window.maximize();
+    window.loadFile(join(__dirname, "profiles.html"));
 }
 
 function run() {
     createWindow();
+    ipcMain.on("play", play);
+    window.webContents.send("addProfile", {name: "testname", description: "test", id: 1});
+    window.webContents.send("addProfile", {name: "testname2", description: "test2", id: 2});
 }
 
 app.on("ready", run);
 app.on("window-all-closed", app.quit);
-app.on("activate", createWindow);
+app.on('browser-window-focus', function () {
+    globalShortcut.register("CommandOrControl+R", () => {});
+    globalShortcut.register("F5", () => {});
+});
