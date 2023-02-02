e77e797f85c063000ffb7bdc4b6ad7751aa39556
MrStupsi <MrStupsi@gmx.de>
2023-02-02 17:46:25 +0100

    profiles file

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
-const { assert } = require("console");
-const { app, BrowserWindow, ipcMain, globalShortcut } = require("electron");
-const { join } = require("path");
-var window = null;
+const { assert } = require("console")
+const { app, BrowserWindow, ipcMain, globalShortcut } = require("electron")
+const { readFileSync, writeFileSync } = require("fs")
+const { join } = require("path")
 
-function play(event, data) {
-    assert(window != null);
-    console.log("play: " + data);
+var window = null
+
+function getProfiles() {
+    try {return JSON.parse(readFileSync(join(__dirname, "profiles.json")))}
+    catch (e) {
+        writeFileSync(join(__dirname, "profiles.json"), "[]")
+        return getProfiles()
+    }
 }
 
 function createWindow() {
@@ -14,22 +19,28 @@ function createWindow() {
             nodeIntegration: true,
             preload: join(__dirname, "preload.js")
         }
-    });
-    window.setMenuBarVisibility(true);
-    window.maximize();
-    window.loadFile(join(__dirname, "profiles.html"));
+    })
+    window.setMenuBarVisibility(true)
+    window.maximize()
+    window.loadFile(join(__dirname, "profiles.html"))
+    const profiles = getProfiles();
+    profiles.forEach(profile => window.webContents.send("addProfile", profile))
+    console.log(profiles)
+}
+
+function play(event, data) {
+    assert(window != null)
+    console.log("play: " + data)
 }
 
 function run() {
-    createWindow();
-    ipcMain.on("play", play);
-    window.webContents.send("addProfile", {name: "testname", description: "test", id: 1});
-    window.webContents.send("addProfile", {name: "testname2", description: "test2", id: 2});
+    createWindow()
+    ipcMain.on("play", play)
 }
 
-app.on("ready", run);
-app.on("window-all-closed", app.quit);
+app.on("ready", run)
+app.on("window-all-closed", app.quit)
 app.on('browser-window-focus', function () {
-    globalShortcut.register("CommandOrControl+R", () => {});
-    globalShortcut.register("F5", () => {});
-});
+    globalShortcut.register("CommandOrControl+R", () => {})
+    globalShortcut.register("F5", () => {})
+})
