748b8938bbfc665072525007eb3e609a8f2147ad
MrStupsi <66797305+MrStupsi@users.noreply.github.com>
2023-02-08 20:59:48 +0100

    added function installMod

--- src/mods.js
+++ src/mods.js
@@ -28,18 +28,27 @@ module.exports.loadMods = (mods, callback) => {
     })
 }
 
+module.exports.installMod = (mod, mods, installedMods) => {
+    if (!mod) return
+    mod.dependencies.forEach((dependency) => {
+        this.installMod(this.getModById(dependency, mods))
+    })
+    const modFile = mod.url.substring(mod.url.lastIndexOf("/") + 1)
+    if (modFile in installedMods) installedMods.splice(installedMods.indexOf(mod.id))
+    else if (existsSync(join(modsdir(), modFile))) return
+    else {
+        const cacheFile = join(modscache(), modFile)
+        if (!existsSync(cacheFile)) execSync("curl -o \"" + cacheFile + "\" \"" + mod.url + "\"")
+        copyFileSync(cacheFile, join(modsdir(), modFile))
+        installedMods.splice(installedMods.indexOf(mod.id))
+    }
+}
+
 module.exports.installMods = (profile, mods) => {
     const installedMods = readdirSync(modsdir())
     profile.mods.forEach((mod) => {
         mod = this.getModById(mod, mods)
-        const modFile = mod.url.substring(mod.url.lastIndexOf("/") + 1)
-        if (modFile in installedMods) installedMods.splice(installedMods.indexOf(mod.id))
-        else {
-            const cacheFile = join(modscache(), modFile)
-            if (!existsSync(cacheFile)) execSync("curl -o \"" + cacheFile + "\" \"" + mod.url + "\"")
-            copyFileSync(cacheFile, join(modsdir(), modFile))
-            installedMods.splice(installedMods.indexOf(mod.id))
-        }
+        this.installMod(mod, mods, this.installMods)
     })
     installedMods.forEach(mod => unlinkSync(join(modsdir(), mod)))
 }
